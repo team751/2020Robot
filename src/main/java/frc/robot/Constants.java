76b7946e18a3ef0b4919d663ebd59d19d35329fc
Dimitri <dsaliba20@priorypanther.com>
2020-02-29 16:16:09 -0800

    I am crying

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -105,8 +105,15 @@ public final class Constants {
      /===========Lightstrip=================/
     /=====================================*/
     public static int LEDPort = 0;
-    public static int LEDLength = 15;
-    public static Orientation LEDOrientation = Orientation.HORIZONTAL;
+    public static int LEDLength = 28;
+
+    public static int FTLEDstart = 0;
+    public static int FTLEDLength = 15;
+    public static Orientation LFTEDOrientation = Orientation.HORIZONTAL;
+
+    public static int FBLEDstart = 16;
+    public static int FBLEDLength = 28;
+    public static Orientation FBLEDOrientation = Orientation.HORIZONTAL;
 
 
     // Robot-specific PIDTrajectory constants 
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -11,6 +11,7 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.core751.subsystems.LightStrip;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -32,6 +33,8 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
+    LightStrip.start();
+    LightStrip.initializeStrip(Constants.LEDPort, Constants.LEDLength);
   }
 
   /**
@@ -48,6 +51,7 @@ public class Robot extends TimedRobot {
     // and running subsystem periodic() methods.  This must be called from the robot's periodic
     // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run();
+    LightStrip.pushBuffer();
   }
 
   /**
